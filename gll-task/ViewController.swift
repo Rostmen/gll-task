8541939c3399d6306d5530d1699b8e752ab738a5
Rostyslav Kobizsky <kobizsky@gmail.com>
2016-05-15 13:57:03 +0300

    Initial Commit

---
+++ gll-task/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gll-task
+//
+//  Created by Rostyslav Kobizsky on 5/15/16.
+//  Copyright © 2016 Rostyslav Kobizsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
